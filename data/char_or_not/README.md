6fd98aaf519c92bf831f2307eb3b69efdde6e284
Kazato Sugimoto <uiureo@gmail.com>
2016-07-06 16:02:52 +0900

    Remove data/ from git repository

--- data/char_or_not/README.md
+++
@@ -1,3 +0,0 @@
-This data is generated from [Street View Text dataset](http://vision.ucsd.edu/~kai/svt/).
-
-http://vision.ucsd.edu/~kai/svt/
--- extract_mser.py
+++ extract_mser.py
@@ -10,8 +10,6 @@ root = ElementTree.parse(data_dir + 'train.xml').getroot()
 
 images = [image.find('imageName').text for image in root]
 
-images = images[0:2]
-
 for image in images:
     img = cv2.imread(data_dir + image)
     rois = mser.mser_rois(img)
@@ -20,5 +18,5 @@ for image in images:
 
     i = 0
     for roi in rois:
-        cv2.imwrite('tmp2/%s_%d.png' % (base, i), roi)
+        cv2.imwrite('tmp/%s_%d.png' % (base, i), roi)
         i += 1
