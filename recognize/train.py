ab80df91c13a8de2a5054247f6060ddf9475e1dd
Kazato Sugimoto <uiureo@gmail.com>
2016-07-08 18:53:16 +0900

    Implement early stopping: 0.698

--- recognize/train.py
+++ recognize/train.py
@@ -23,7 +23,6 @@ sess = tf.Session()
 merged = tf.merge_all_summaries()
 writer = tf.train.SummaryWriter("./tmp_tensorflow/recognize/logs", sess.graph_def)
 
-
 accuracy = model.evaluate(predictions, y_)
 
 sess.run(tf.initialize_all_variables())
@@ -33,7 +32,9 @@ test_images, test_labels = sess.run(load_test_op)
 
 start_time = time.time()
 
-for i in range(10000):
+i = 0
+prev_test_accuracy = 0.0
+while True:
   batch = sess.run(batch_op)
 
   if i % 100 == 0:
@@ -42,12 +43,24 @@ for i in range(10000):
 
       writer.add_summary(m, i)
 
-      print('epoch: %d  train: %0.04f   test: %0.04f    time: %0.03f' % (i, train_accuracy, test_accuracy, time.time() - start_time))
+      print('epoch:%d  train:%0.04f   test:%0.04f    time:%0.03f' % (i, train_accuracy, test_accuracy, time.time() - start_time))
+
       start_time = time.time()
 
   sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-  if (i+1) % 1000 == 0:
-     saver.save(sess, './tmp_tensorflow/recognize/20160708', global_step=(i + 1))
+  if (i-1) % 1000 == 0:
+      saver.save(sess, './tmp_tensorflow/recognize/20160708', global_step=(i + 1))
 
-print(sess.run(accuracy, feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0 }))
+      test_accuracy = sess.run(accuracy, feed_dict={x: test_images, y_: test_labels, keep_prob: 1.0})
+
+      # early stopping
+      if abs(test_accuracy - prev_test_accuracy) < prev_test_accuracy * 0.05:
+          break
+
+      if i+1 > 10000:
+          break
+
+      prev_test_accuracy = test_accuracy
+
+  i += 1
